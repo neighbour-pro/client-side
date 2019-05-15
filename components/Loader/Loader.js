ec5f7744e53840fd2451c70fc29c3f5098a9a03b
AnthonySBR <asbr1811@gmail.com>
2019-05-15 16:51:46 +0200

    Creating routes and enviromental variables

--- components/Loader/Loader.js
+++ components/Loader/Loader.js
@@ -4,8 +4,8 @@ import { Text, View } from 'react-native'
 export default class Loader extends Component {
     render() {
         return (
-            <View style={{}}>
-                <Text>Loading...</Text>
+            <View style={{flex:1, paddingTop: 180, paddingLeft: 80}}>
+                <Text>{process.env.REACT_APP_SERVER_URL}</Text>
             </View>
         )
     }
