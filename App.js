2ab39bf3e466236af6ba34237482e3bd6f7e7ef8
Raúl Contreras <raulc98@gmail.com>
2019-06-03 22:15:40 +0200

    Remove complete application

--- App.js
+++ App.js
@@ -7,79 +7,24 @@
  */
 
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-import AuthService from './services/AuthService';
+import { StyleSheet, StatusBar, Text, View } from 'react-native';
 
-import Nav from './components/Nav/Nav';
-import NavGuest from './components/Nav/NavGuest';
-import Loader from './components/Loader/Loader';
-import ReviewPage from './components/Test/ReviewTest'
-console.disableYellowBox = true
 export default class App extends Component {
 
-  constructor() {
-    super();
-    this.state = {
-      isLoggedIn: false,
-      loaded: false
-    }
-    this.authService = new AuthService();
-  }
-
-  
-  componentDidMount(){
-    this.checkLoggedUser();
-  }
-
-  checkLoggedUser = () =>{
-    this.authService.isLogged()
-    .then(res => {
-      this.setState({
-        ...this.state,
-        loaded: true,
-        isLoggedIn: res.data
-      })
-    })
-    .catch(err => {
-      this.setState({
-        ...this.state,
-        loaded: true,
-        isLoggedIn: false
-      })
-    });
-  }
-
-  changeToLoggedMenu(user){
-    this.setState({
-      ...this.state,
-      isLoggedIn: user,
-      loaded: true
-    })
-  }
-
-  changeToGuestMenu(){
-    this.setState({
-      ...this.state,
-      isLoggedIn: false,
-      loaded: true
-    })
-  }
-  
   render() {
     return (
-      <React.Fragment>
-        {
-          this.state.loaded ?
-            this.state.isLoggedIn ?
-            <Nav changeMenu={()=>this.changeToGuestMenu()} checkLoggedUser={()=>this.checkLoggedUser()}/> :
-            <NavGuest changeMenu={(user)=>this.changeToLoggedMenu(user)}/>
-            :
-            <Loader/>
-        }
-      </React.Fragment>
+      <View style={styles.container}>
+        <Text>Neighbour Pro App</Text>
+      </View>
     );
 
   }
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginVertical: StatusBar.currentHeight,
+    backgroundColor: 'red',
+  }
+})
--- config/config.js
+++
@@ -1,4 +0,0 @@
-export default config = {
-    // SERVER_URL: 'https://neighbour-pro.herokuapp.com'
-    SERVER_URL: 'https://neighbour-pro.herokuapp.com'
-}
